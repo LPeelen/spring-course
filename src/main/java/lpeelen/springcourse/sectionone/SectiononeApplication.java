e7ea53d8625dcdce41e84d8718c6c244e9a2fe3b
Lucas Peelen <lucas.peelen@simacan.com>
2023-05-22 20:26:49 +0200

    Initial commit for new project structure

---
+++ src/main/java/lpeelen/springcourse/sectionone/SectiononeApplication.java
@@ -0,0 +1,13 @@
+package lpeelen.springcourse.sectionone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectiononeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SectiononeApplication.class, args);
+	}
+
+}
